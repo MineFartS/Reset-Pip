5f26cb646cbb46a00d8ec2a55697b5b795544b3e
Phil Hunt <philiphuntiv2@gmail.com>
2025-12-09 16:54:41 -0500

    Clear Pip Cache & __pycache__

--- Run.ps1
+++ Run.ps1
@@ -4,3 +4,30 @@ $req = "$env:TEMP\req.txt"
 pip.exe freeze > $req
 
 pip.exe uninstall -r $req -y
+
+pip.exe cache purge
+
+Get-PSDrive `
+    -PSProvider FileSystem `
+    `
+| Select-Object `
+    -ExpandProperty Name `
+    `
+| ForEach-Object -Process {
+
+    Get-ChildItem `
+        -Path "$($_):\" `
+        -Recurse `
+        -Directory `
+        -ErrorAction SilentlyContinue `
+    | Where-Object Name -eq "__pycache__" `
+    | ForEach-Object -Process {
+
+        Remove-Item `
+            -Path $_.FullName `
+            -Force -Recurse -Verbose
+        
+    }
+
+
+}
